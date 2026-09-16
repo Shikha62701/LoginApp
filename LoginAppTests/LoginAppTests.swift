483716855b75c7ba02f9826322094c8c8fb2ac4c
shikha singh <kumkum9151@gmail.com>
2026-09-16 12:40:37 +0530

    Initial Commit

---
+++ LoginAppTests/LoginAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  LoginAppTests.swift
+//  LoginAppTests
+//
+//  Created by shikha singh on 16/09/26.
+//
+
+import Testing
+@testable import LoginApp
+
+struct LoginAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
